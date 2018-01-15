f87dea573ee0074cd9d2f57d30d35b0e2239d9e5
Emilio da Costa <emidac@gmail.com>
2018-01-15 16:55:35 -0500

    logging middleware

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const app = express();
 app.listen(3000, () => console.log('Twitter app listening for tweets on port 3000'));
 
 app.get('/', (request, response) => response.send('I am responding'));
+
+app.use('/*', function (req, res, next) {
+  console.log("words")
+  next()
+})
