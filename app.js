aa6a32ebe01812bb6f6da056c62c6ef7b619d845
Emilio da Costa <emidac@gmail.com>
2018-01-15 16:45:57 -0500

    Responding

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require( 'express' );
+const app = express();
+
+app.listen(3000, () => console.log('Twitter app listening for tweets on port 3000'));
+
+app.get('/', (request, response) => response.send('I am responding'));
