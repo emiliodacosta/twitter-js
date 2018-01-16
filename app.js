5cefc1055dd87bc2d9f7fe9d136ef5702f97d66d
Emilio da Costa <emidac@gmail.com>
2018-01-15 23:48:30 -0500

    added middleware and nodemon

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.listen(3000, () => console.log('Twitter app listening for tweets on port 3000'));
 
-app.get('/', (request, response) => response.send('I am responding'));
-
-app.use('/*', function (req, res, next) {
-  console.log("words")
-  next()
+app.use(function (req, res, next) {
+  console.log(req.method, req.url);
+  next();
 })
+
+app.get('/', (req, res) => res.send('I am responding'));
