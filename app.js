3824ab7f73824e1cef7c89d5680d435fcfdb1435
Emilio da Costa <emidac@gmail.com>
2018-01-26 09:48:37 -0500

    testing ssh

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ people: [
   ]
 }
 
+// test for git push
+
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
